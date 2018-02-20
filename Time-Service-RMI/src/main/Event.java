60f48597acab617238ea94d62691d866ed84b5b7
KingMus <marcofm47@gmail.com>
2018-02-20 15:44:54 +0100

    added method logic

--- Time-Service-RMI/src/main/Event.java
+++ Time-Service-RMI/src/main/Event.java
@@ -1,5 +1,7 @@
 package main;
 
-public class Event {
+import java.util.Date;
+
+public class Event extends Date {
 
 }
--- Time-Service-RMI/src/main/TimeService.java
+++ Time-Service-RMI/src/main/TimeService.java
@@ -7,41 +7,56 @@ import java.util.Vector;
 
 public class TimeService extends UnicastRemoteObject implements TimeServiceInterface {
 
+	private Vector<Event> allEvents = new Vector<Event>();
+
 	protected TimeService() throws RemoteException {
 	}
 
 	public Date getDateAndTime() throws RemoteException {
 
 		System.out.println("success");
-
 		Date date = new Date();
-
 		return date;
 
 	}
 
 	@Override
 	public void addEvent(Event event) throws RemoteException {
-		// TODO Auto-generated method stub
-
+		allEvents.addElement(event);
 	}
 
 	@Override
 	public Vector<Event> getAllEvents() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return allEvents;
 	}
 
 	@Override
 	public Event getNextEvent() throws RemoteException {
-		// TODO Auto-generated method stub
+
+		Date date = new Date();
+
+		for (Event event : allEvents) {
+			if (date.getTime() < event.getTime()) {
+				return event;
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public Vector<Event> getFutureEvents() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+
+		Date date = new Date();
+		Vector<Event> allFutureEvents = new Vector<Event>();
+
+		for (Event event : allEvents) {
+			if (date.getTime() < event.getTime()) {
+				allFutureEvents.addElement(event);
+			}
+		}
+
+		return allFutureEvents;
 	}
 
 }
