d5e3e6485e58488d4f096bb668df6d434a9027cf
KingMus <marcofm47@gmail.com>
2018-02-20 10:27:58 +0100

    created registry

--- Time-Service-RMI/src/main/TimeServiceRegistry.java
+++ Time-Service-RMI/src/main/TimeServiceRegistry.java
@@ -1,5 +1,17 @@
 package main;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class TimeServiceRegistry {
+
+	public static void main(String[] args) throws Exception {
+
+		Registry registry = LocateRegistry.createRegistry(8080);
+		TimeService timeService = new TimeService();
+		registry.bind("TS", timeService);
+		System.out.println("RMI started on port 8080");
+
+	}
 
 }
